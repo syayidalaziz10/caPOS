93361f3f74eade87e5516bfb454c95a00f0fc94a
syayidalaziz10 <shorashiro15@gmail.com>
2022-11-14 22:09:45 +0700

    menambahkan menu login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Login</title>
+</head>
+<body>
+    <div class="h-screen md:flex">
+        <div class="relative overflow-hidden md:flex w-1/2 justify-around items-center">
+            <h1>Login</h1>
+        </div>
+        <div class="relative overflow-hidden md:flex w-1/2 justify-around items-center hidden">
+            <img src="img/login-bg.jpg" alt="bg-side-img">
+        </div>
+    </div>
+</body>
+</html>
